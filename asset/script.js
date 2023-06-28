b9b392c60a8df2899c7b9fd668e4d79dd753b096
foxeyb28 <131126615+foxeyb28@users.noreply.github.com>
2023-06-28 00:08:19 -0500

    added search button and more links to html

---
+++ asset/script.js
@@ -0,0 +1,17 @@
+var searchResult = document.querySelector("#movieResult");
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems, {});
+});
+
+var testData;
+var apiKey = 'de601ddd2d309aebbd9d11f32fbaa2bd';
+var ;
+var userInput;
+var weatherApiKey = {
+  method: 'GET',
+  headers; {
+    'OpenWeatherAPI-Key': 'de601ddd2d309aebbd9d11f32fbaa2bd',
+    'OpenWeatherAPI-Host': 'OpenWeathermap.org'
+  }
+};
