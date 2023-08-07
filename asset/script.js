2265890696a9a0b864100bc1e26c3c2a93e6ed7f
foxeyb28 <131126615+foxeyb28@users.noreply.github.com>
2023-08-07 06:35:32 -0500

    added use of local storage and styleing in css

--- asset/script.js
+++ asset/script.js
@@ -2,15 +2,27 @@ var searchResult = document.querySelector("#weather");
 document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.dropdown-trigger');
   var instances = M.Dropdown.init(elems, {});
+  localStorage.setItem("elems", instances);
 });
 
+function renderLastRegistered() {
+  var email = localStorage.getItem("email");
+  var password = localStorage.getItem("password");
+
+  if (!email || !password) {
+    return;
+  }
+ 
 var testData;
 var apiKey = 'de601ddd2d309aebbd9d11f32fbaa2bd';
 var userInput;
 var weatherApiKey = {
   method: 'GET',
-  headers; {
+  headers: {
     'OpenWeatherAPI-Key': 'de601ddd2d309aebbd9d11f32fbaa2bd',
     'OpenWeatherAPI-Host': 'OpenWeathermap.org'
   }
 };
+localStorage.setItem("elems", instances);
+localStorage.setItem("elems", instances);
+renderLastRegistered();
