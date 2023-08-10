18e150deb01c6b11c166f9b1fb4902c907fe3bff
foxeyb28 <131126615+foxeyb28@users.noreply.github.com>
2023-08-10 15:48:09 -0500

    added more code to index and js

--- asset/script.js
+++ asset/script.js
@@ -1,4 +1,7 @@
 var searchResult = document.querySelector("#weather");
+var searchBtn = document.querySelector("#searchBtn");
+var inputCity = document.querySelector("#City");
+var currentWeather = document.querySelector("#currentWeather")
 document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.dropdown-trigger');
   var instances = M.Dropdown.init(elems, {});
@@ -12,7 +15,7 @@ function renderLastRegistered() {
   if (!email || !password) {
     return;
   }
- 
+}
 var testData;
 var apiKey = 'de601ddd2d309aebbd9d11f32fbaa2bd';
 var userInput;
@@ -23,6 +26,26 @@ var weatherApiKey = {
     'OpenWeatherAPI-Host': 'OpenWeathermap.org'
   }
 };
-localStorage.setItem("elems", instances);
-localStorage.setItem("elems", instances);
+function getWeather(city){
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`)
+  .then((response)=>{
+    console.log(response)
+    return response.json()
+  }) .then((data)=>{
+    console.log(data)
+    var h1 = document.createElement("h1")
+    h1.textContent = data.name
+currentWeather.append(h1)
+var temp = document.createElement("p")
+temp.textContent = data.main.temp
+currentWeather.append(temp)
+  })
+}
+//localStorage.setItem("elems", instances);
+//localStorage.setItem("elems", instances);
 renderLastRegistered();
+searchBtn.addEventListener("click", function(){
+  var userSearch = inputCity.value 
+  console.log(userSearch)
+  getWeather(userSearch)
+})
