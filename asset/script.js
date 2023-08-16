ddb74fc11357c33bb2dc4fd63b617a651da4ebe6
foxeyb28 <131126615+foxeyb28@users.noreply.github.com>
2023-08-16 01:07:09 -0500

    added coding for forecast

--- asset/script.js
+++ asset/script.js
@@ -2,20 +2,13 @@ var searchResult = document.querySelector("#weather");
 var searchBtn = document.querySelector("#searchBtn");
 var inputCity = document.querySelector("#City");
 var currentWeather = document.querySelector("#currentWeather")
+var forecast = document.querySelector("#forecast");
 document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.dropdown-trigger');
   var instances = M.Dropdown.init(elems, {});
   localStorage.setItem("elems", instances);
 });
 
-function renderLastRegistered() {
-  var email = localStorage.getItem("email");
-  var password = localStorage.getItem("password");
-
-  if (!email || !password) {
-    return;
-  }
-}
 var testData;
 var apiKey = 'de601ddd2d309aebbd9d11f32fbaa2bd';
 var userInput;
@@ -39,13 +32,27 @@ currentWeather.append(h1)
 var temp = document.createElement("p")
 temp.textContent = data.main.temp
 currentWeather.append(temp)
-  })
-}
-//localStorage.setItem("elems", instances);
-//localStorage.setItem("elems", instances);
-renderLastRegistered();
-searchBtn.addEventListener("click", function(){
-  var userSearch = inputCity.value 
-  console.log(userSearch)
-  getWeather(userSearch)
-})
+  })};
+function getForecast(city){
+ fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`)
+  .then((response)=>{
+   console.log(response)
+   return response.json()
+ }) .then((data)=>{
+   console.log(data)
+//    var h2 = document.createElement("h2")
+//    h1.textContent = data.name
+// forecast.append(h2)
+// var = document.createElement("p")
+// temp.textContent = data.main.forecast
+// forecast.append(temp)
+ })};
+  
+ 
+ 
+ searchBtn.addEventListener("click", function(){
+   var userSearch = inputCity.value 
+   console.log(userSearch)
+   getWeather(userSearch)
+   getForecast(userSearch)
+  });
